ec65f7133210569d23bbdf8b12ff6445b3d05682
willamy388720 <willamygt@gmail.com>
2021-04-20 18:14:19 -0300

    Model Contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :contacts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'static_pages#index'
-  get "/about", to: 'static_pages#about'
-  get "/contact", to: 'static_pages#contact'
+  get '/about', to: 'static_pages#about'
+  get '/contato', to: 'static_pages#contact'
 end
