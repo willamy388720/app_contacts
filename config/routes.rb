552320aea1bcba0bf7d6187e15469b6d8aae28ff
willamy388720 <willamygt@gmail.com>
2021-04-20 19:48:51 -0300

    Validates

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :contacts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'static_pages#index'
+
   get '/about', to: 'static_pages#about'
   get '/contato', to: 'static_pages#contact'
+
+  resources :contacts
+  resources :users, only: %i[new create]
 end
