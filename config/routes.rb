a84d6cfa47f64c8ecfcc3c8aab7a22d552bf0436
willamy388720 <willamygt@gmail.com>
2021-04-20 21:36:23 -0300

    User authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'static_pages#index'
 
   get '/about', to: 'static_pages#about'
   get '/contato', to: 'static_pages#contact'
+  get '/entrar', to: 'sessions#new'
+  post '/entrar', to: 'sessions#create'
+  delete '/sair', to: 'sessions#destroy'
 
   resources :contacts
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create show]
 end
